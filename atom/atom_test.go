c6a4b12989e895b472e57a1e4f5fa89d90852396
John Southworth <john.southworth@gmail.com>
2019-03-04 20:19:20 -0800

    atom: Implement a more comprehensive test

--- atom/atom_test.go
+++ atom/atom_test.go
@@ -1,6 +1,7 @@
 package atom
 
 import (
+	"sync"
 	"testing"
 )
 
@@ -10,12 +11,52 @@ func TestSwap(t *testing.T) {
 	if b != 1 {
 		t.Fatalf("got %v, wanted %v\n", b, 1)
 	}
-	a.Swap(func(cur interface{}, args ...interface{}) interface{} {
+	a.Swap(func(cur int) int {
 		return 2
 	})
 	b = a.Deref()
 	if b != 2 {
 		t.Fatalf("got %v, wanted %v\n", b, 2)
+	}
+}
+
+func TestConcurrent(t *testing.T) {
+	var wg sync.WaitGroup
+	wait := make(chan struct{})
+	executed := make(chan int, 100)
+	a := New(1)
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go func(i int) {
+			<-wait
+			count := 0
+			a.Swap(func(cur int) int {
+				count++
+				return i + cur
+			})
+			executed <- count
+			wg.Done()
+		}(i)
+	}
+
+	go func() {
+		wg.Wait()
+		close(executed)
+	}()
+
+	close(wait)
+
+	total := 0
+	for count := range executed {
+		total += count
+	}
+	if total <= 100 {
+		t.Fatal("there was no contention on the atom")
+	}
+
+	val := a.Deref()
+	if val != 4951 {
+		t.Fatalf("didn't get expected result, got %s, wanted 4951", val)
 	}
 }
 
