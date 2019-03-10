f157ca4e66341bac259004ed78f7c5eee21fda18
John Southworth <john.southworth@gmail.com>
2019-03-10 14:34:04 -0700

    agent: add documenation

--- agent/agent.go
+++ agent/agent.go
@@ -1,3 +1,9 @@
+// Package agent implements a mechanism to manage a single piece of
+// state. It operates asynchronously to the rest of the program. It does
+// not use a blocking goroutine for this, instead it spins up a new
+// goroutine as needed and terminates the routine when its queue of work
+// is empty. Accessing the value of an agent does not require
+// coordination with other accessors or with updaters.
 package agent
 
 import (
@@ -8,11 +14,13 @@ import (
 	"jsouthworth.net/go/immutable/queue"
 )
 
+// Agent is a mechanism to manage a single piece of state.
 type Agent struct {
 	state *atom.Atom
 	queue ref.Ref
 }
 
+// New returns a new agent with an initial value of s.
 func New(s interface{}) *Agent {
 	return &Agent{
 		state: atom.New(s),
@@ -20,6 +28,10 @@ func New(s interface{}) *Agent {
 	}
 }
 
+// Send dispatches an action. It returns immediately and the value
+// managed by the agent will be updated asynchronusly. Send takes a
+// function of the type func(old aT, args...) rT where aT is the old
+// type of the agent and rT is the desired type of the atom.
 func (a *Agent) Send(fn interface{}, args ...interface{}) *Agent {
 	action := &agentRequest{fn: fn, args: args}
 	var prev unsafe.Pointer
@@ -36,6 +48,7 @@ func (a *Agent) Send(fn interface{}, args ...interface{}) *Agent {
 	return a
 }
 
+// Deref returns the current value of the agent.
 func (a *Agent) Deref() interface{} {
 	return a.state.Deref()
 }
