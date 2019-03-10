7f9eb19d652f9a24cea2fd50e299963d55554ac5
John Southworth <john.southworth@gmail.com>
2019-03-10 12:44:09 -0700

    agent: rework to use immutable/queue and unsafe/ref

--- agent/agent.go
+++ agent/agent.go
@@ -1,33 +1,64 @@
 package agent
 
 import (
-	"github.com/jsouthworth/etm/atom"
-)
+	"unsafe"
 
-type agentRequest struct {
-	fn   atom.XfrmFn
-	args []interface{}
-}
+	"jsouthworth.net/go/etm/atom"
+	"jsouthworth.net/go/etm/unsafe/ref"
+	"jsouthworth.net/go/immutable/queue"
+)
 
 type Agent struct {
 	state *atom.Atom
-	ch    chan agentRequest
+	queue ref.Ref
 }
 
 func New(s interface{}) *Agent {
-	a := &Agent{state: atom.New(s), ch: make(chan agentRequest, 100)}
-	go a.process()
-	return a
-}
-
-func (a *Agent) Send(fn atom.XfrmFn, args ...interface{}) *Agent {
-	a.ch <- agentRequest{fn: fn, args: args}
-	return a
-}
-
-func (a *Agent) process() {
-	for {
-		r := <-a.ch
-		a.state.Swap(r.fn, r.args...)
+	return &Agent{
+		state: atom.New(s),
+		queue: ref.Make(unsafe.Pointer(queue.Empty())),
 	}
+}
+
+func (a *Agent) Send(fn interface{}, args ...interface{}) *Agent {
+	action := &agentRequest{fn: fn, args: args}
+	var prev unsafe.Pointer
+	for {
+		prev = a.queue.Load()
+		new := unsafe.Pointer((*queue.Queue)(prev).Push(action))
+		if a.queue.CompareAndSwap(prev, new) {
+			break
+		}
+	}
+	if (*queue.Queue)(prev).Length() == 0 {
+		go a.run()
+	}
+	return a
+}
+
+func (a *Agent) Deref() interface{} {
+	return a.state.Deref()
+}
+
+func (a *Agent) run() {
+	for {
+		action := (*queue.Queue)(a.queue.Load()).First().(*agentRequest)
+		a.state.Swap(action.fn, action.args...)
+		var next *queue.Queue
+		for {
+			prev := a.queue.Load()
+			next = (*queue.Queue)(prev).Pop()
+			if a.queue.CompareAndSwap(prev, unsafe.Pointer(next)) {
+				break
+			}
+		}
+		if next.Length() == 0 {
+			break
+		}
+	}
+}
+
+type agentRequest struct {
+	fn   interface{}
+	args []interface{}
 }
